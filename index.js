23ae643afc6faf33224f313289016cd4e1af5909
aleks <aleksgnu@gmail.com>
2019-03-27 03:12:33 -0600

    Create users mondel and send first user to db 🙂

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
 const JwtStrategy = passportJWT.Strategy;
 const ExtractJwt = passportJWT.ExtractJwt;
+
+const knex = require('knex');
+const knexDb = knex({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING
+});
+const bookshelf = require('bookshelf');
+const securePassword = require('bookshelf-secure-password');
+const db = bookshelf(knexDb);
+
+db.plugin(securePassword);
+
+const User = db.Model.extend({
+  tableName: 'users',
+  hasSecurePassword: true
+});
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -15,11 +31,38 @@ const opts = {
 
 const strategy = new JwtStrategy(opts, (payload, next) => {
   // GET user from DB
-  const user = null;
-  next(null, user);
+  User.forge({ id: payload.id })
+    .fetch()
+    .then(res => {
+      next(null, res);
+    });
 });
+
 passport.use(strategy);
 app.use(passport.initialize());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+app.post('/seedUser', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(401).send('no fields');
+  }
+
+  console.log(req.body);
+
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user.save().then(() => {
+    res.send('ok');
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('J W T');
--- migrations/20190327003902_create_user_table.js
+++ migrations/20190327003902_create_user_table.js
@@ -1,11 +1,11 @@
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('login_user', t => {
+  return knex.schema.createTable('users', t => {
     t.increments('id').unsigned().primary()
     t.string('email').notNull()
-    t.string('password').notNull()
+    t.string('password_digest').notNull()
   })
 };
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('login_user')
+  return knex.schema.dropTable('users');
 };
